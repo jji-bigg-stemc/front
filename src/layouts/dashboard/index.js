acb39124f0e5f341c665f41fb0bd19938a75afd2
jerry2793 <ruiyang-jerry@outlook.com>
2022-10-17 21:51:49 -0700

    new icons on stats cards

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -42,21 +42,9 @@ import OrderOverview from "layouts/dashboard/components/OrderOverview";
 import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
 import gradientLineChartData from "layouts/dashboard/data/gradientLineChartData";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 function Dashboard() {
-  const { size } = typography;
-  const { chart, items } = reportsBarChartData;
-
-  return (
-    <div>
-      do this one later; the original is below but is broken
-      {/* <DashBoardOriginal /> */}
-    </div>
-  );
-}
-
-export default Dashboard;
-
-function DashBoardOriginal() {
   const { size } = typography;
   const { chart, items } = reportsBarChartData;
 
@@ -68,36 +56,49 @@ function DashBoardOriginal() {
           <Grid container spacing={3}>
             <Grid item xs={12} sm={6} xl={3}>
               <MiniStatisticsCard
-                title={{ text: "today's money" }}
+                title={{ text: "google classroom" }}
                 count="$53,000"
                 percentage={{ color: "success", text: "+55%" }}
-                icon={{ color: "info", component: "paid" }}
+                icon={{
+                  color: "info",
+                  component: (
+                    <FontAwesomeIcon icon="fa-brands fa-google-drive" />
+                  ),
+                }}
               />
             </Grid>
             <Grid item xs={12} sm={6} xl={3}>
               <MiniStatisticsCard
-                title={{ text: "today's users" }}
+                title={{ text: "remind" }}
                 count="2,300"
                 percentage={{ color: "success", text: "+3%" }}
-                icon={{ color: "info", component: "public" }}
+                icon={{
+                  color: "info",
+                  component: (
+                    <FontAwesomeIcon icon="fa-solid fa-thought-bubble" />
+                  ),
+                }}
               />
             </Grid>
             <Grid item xs={12} sm={6} xl={3}>
               <MiniStatisticsCard
-                title={{ text: "new clients" }}
+                title={{ text: "discord" }}
                 count="+3,462"
                 percentage={{ color: "error", text: "-2%" }}
-                icon={{ color: "info", component: "emoji_events" }}
+                icon={{
+                  color: "info",
+                  component: <FontAwesomeIcon icon="fa-brands fa-discord" />,
+                }}
               />
             </Grid>
             <Grid item xs={12} sm={6} xl={3}>
               <MiniStatisticsCard
-                title={{ text: "sales" }}
+                title={{ text: "instagram" }}
                 count="$103,430"
                 percentage={{ color: "success", text: "+5%" }}
                 icon={{
                   color: "info",
-                  component: "shopping_cart",
+                  component: <FontAwesomeIcon icon="fa-brands fa-instagram" />,
                 }}
               />
             </Grid>
@@ -113,66 +114,135 @@ function DashBoardOriginal() {
             </Grid>
           </Grid>
         </SoftBox>
-        <SoftBox mb={3}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} lg={5}>
-              <ReportsBarChart
-                title="active users"
-                description={
-                  <>
-                    (<strong>+23%</strong>) than last week
-                  </>
-                }
-                chart={chart}
-                items={items}
-              />
-            </Grid>
-            <Grid item xs={12} lg={7}>
-              <GradientLineChart
-                title="Sales Overview"
-                description={
-                  <SoftBox display="flex" alignItems="center">
-                    <SoftBox
-                      fontSize={size.lg}
-                      color="success"
-                      mb={0.3}
-                      mr={0.5}
-                      lineHeight={0}
-                    >
-                      <Icon className="font-bold">arrow_upward</Icon>
-                    </SoftBox>
-                    <SoftTypography
-                      variant="button"
-                      color="text"
-                      fontWeight="medium"
-                    >
-                      4% more{" "}
-                      <SoftTypography
-                        variant="button"
-                        color="text"
-                        fontWeight="regular"
-                      >
-                        in 2021
-                      </SoftTypography>
-                    </SoftTypography>
-                  </SoftBox>
-                }
-                height="20.25rem"
-                chart={gradientLineChartData}
-              />
-            </Grid>
-          </Grid>
-        </SoftBox>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6} lg={8}>
-            <Projects />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <OrderOverview />
-          </Grid>
-        </Grid>
       </SoftBox>
-      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Dashboard;
+
+// the original version of the dashboard I am cutting through
+// function Dashboard() {
+//   const { size } = typography;
+//   const { chart, items } = reportsBarChartData;
+
+//   return (
+//     <DashboardLayout>
+//       <DashboardNavbar />
+//       <SoftBox py={3}>
+//         <SoftBox mb={3}>
+//           <Grid container spacing={3}>
+//             <Grid item xs={12} sm={6} xl={3}>
+//               <MiniStatisticsCard
+//                 title={{ text: "today's money" }}
+//                 count="$53,000"
+//                 percentage={{ color: "success", text: "+55%" }}
+//                 icon={{ color: "info", component: "paid" }}
+//               />
+//             </Grid>
+//             <Grid item xs={12} sm={6} xl={3}>
+//               <MiniStatisticsCard
+//                 title={{ text: "today's users" }}
+//                 count="2,300"
+//                 percentage={{ color: "success", text: "+3%" }}
+//                 icon={{ color: "info", component: "public" }}
+//               />
+//             </Grid>
+//             <Grid item xs={12} sm={6} xl={3}>
+//               <MiniStatisticsCard
+//                 title={{ text: "new clients" }}
+//                 count="+3,462"
+//                 percentage={{ color: "error", text: "-2%" }}
+//                 icon={{ color: "info", component: "emoji_events" }}
+//               />
+//             </Grid>
+//             <Grid item xs={12} sm={6} xl={3}>
+//               <MiniStatisticsCard
+//                 title={{ text: "sales" }}
+//                 count="$103,430"
+//                 percentage={{ color: "success", text: "+5%" }}
+//                 icon={{
+//                   color: "info",
+//                   component: "shopping_cart",
+//                 }}
+//               />
+//             </Grid>
+//           </Grid>
+//         </SoftBox>
+//         <SoftBox mb={3}>
+//           <Grid container spacing={3}>
+//             <Grid item xs={12} lg={7}>
+//               <BuildByDevelopers />
+//             </Grid>
+//             <Grid item xs={12} lg={5}>
+//               <WorkWithTheRockets />
+//             </Grid>
+//           </Grid>
+//         </SoftBox>
+//         <SoftBox mb={3}>
+//           <Grid container spacing={3}>
+//             <Grid item xs={12} lg={5}>
+//               <ReportsBarChart
+//                 title="active users"
+//                 description={
+//                   <>
+//                     (<strong>+23%</strong>) than last week
+//                   </>
+//                 }
+//                 chart={chart}
+//                 items={items}
+//               />
+//             </Grid>
+//             <Grid item xs={12} lg={7}>
+//               <GradientLineChart
+//                 title="Sales Overview"
+//                 description={
+//                   <SoftBox display="flex" alignItems="center">
+//                     <SoftBox
+//                       fontSize={size.lg}
+//                       color="success"
+//                       mb={0.3}
+//                       mr={0.5}
+//                       lineHeight={0}
+//                     >
+//                       <Icon className="font-bold">arrow_upward</Icon>
+//                     </SoftBox>
+//                     <SoftTypography
+//                       variant="button"
+//                       color="text"
+//                       fontWeight="medium"
+//                     >
+//                       4% more{" "}
+//                       <SoftTypography
+//                         variant="button"
+//                         color="text"
+//                         fontWeight="regular"
+//                       >
+//                         in 2021
+//                       </SoftTypography>
+//                     </SoftTypography>
+//                   </SoftBox>
+//                 }
+//                 height="20.25rem"
+//                 chart={gradientLineChartData}
+//               />
+//             </Grid>
+//           </Grid>
+//         </SoftBox>
+//         <Grid container spacing={3}>
+//           <Grid item xs={12} md={6} lg={8}>
+//             <Projects />
+//           </Grid>
+//           <Grid item xs={12} md={6} lg={4}>
+//             <OrderOverview />
+//           </Grid>
+//         </Grid>
+//       </SoftBox>
+//       <Footer />
+//     </DashboardLayout>
+//   );
+// }
+Footer />
     </DashboardLayout>
   );
 }
